c98f418d777d6b80e4d5540173aa52367b2a85f6
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-05 16:29:35 +0400

    Color model added and registered

--- server/apps/color/admin.py
+++ server/apps/color/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Color
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    ordering = ('name',)
--- server/apps/color/models.py
+++ server/apps/color/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    """Model definition for Color."""
+
+    name = models.CharField(verbose_name='Name', max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Color."""
+
+        verbose_name = 'Color'
+        verbose_name_plural = 'Colors'
+
+    def __str__(self):
+        """Unicode representation of Color."""
+        return self.name
