a6eac7536fcc4ff32f922d039c600641f660eaaa
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-05 15:50:25 +0400

    Core app added

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -15,6 +15,10 @@ from server.settings.components import BASE_DIR
 # Application definition
 
 INSTALLED_APPS = [
+    # Local apps
+    "server.apps.core",
+
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
